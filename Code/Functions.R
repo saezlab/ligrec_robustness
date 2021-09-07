5775f39b063f34c7b7a1e4031545813096528ccd
LeoBurmedi <49957645+LeoBurmedi@users.noreply.github.com>
2021-09-07 11:32:45 +0200

    Added custom solution for cellchat rank
    Cellchat can't be treated like the other rankings because it produces so many interactions of tied value (all top thousand+ have pvalue = 0).  Added a custom function to handle cellchat and made the ability to cut with ties for get_top_n_ranks()

--- Code/Functions.R
+++ Code/Functions.R
@@ -10,11 +10,15 @@
   #' @param method The method for which you would like to extract the top ranked
   #' interactions, as a string.
   #' @param top_n The number of items to return, returns items ranked #1 to #n.
+  #' @param with_ties TRUE or FALSE. An argument passed to slice_min and 
+  #' slice_max respectively. When slicing the top_n, sometimes a cutoff occurs
+  #' between equally ranked items. Should the items also be included, expanding 
+  #' the selection (TRUE), or should preserving top_n be prioritized (FALSE). 
   #'
   #' @return Returns the tibble input cut down to the top n highest ranked
   #' interactions.
   
-  get_top_n_ranks <- function(data_set, method, top_n) {
+  get_top_n_ranks <- function(data_set, method, top_n, with_ties = FALSE) {
     # generate a list that describes how to rank in each method to get what the
     # method considers best
     rank_spec_list <-
@@ -48,7 +52,7 @@
           data_set,
           n = top_n,
           order_by = !!sym(rank_spec_list[[method]]$method_score),
-          with_ties = FALSE
+          with_ties = with_ties
         )
       
       # order_by is assigned the criterion dictated by rank_spec_list
@@ -62,7 +66,7 @@
           data_set,
           n = top_n,
           order_by = !!sym(rank_spec_list[[method]]$method_score),
-          with_ties = FALSE
+          with_ties = with_ties
         )
       
       # order_by is assigned the criterion dictated by rank_spec_list
@@ -1125,6 +1129,92 @@
       return(percentage_overlap)
       
     } #end of function
+  
+  
+# cellchat_rank_overlap()
+{
+  #' Takes get_n_top_ranks outputs that have an LR_ID and determines their 
+  #' overlap, but handles the exceptions cellchat has
+  #' 
+  #' @description Cellchat top_ranks are often overflowing with interactions 
+  #' that have p-values of zero. If you cut without ties in this scenario you
+  #' ignore a great deal of interactions that have the same ranking as your
+  #' candidates. There is also an ordering bias in the data, such that you
+  #' always receive the same 500 interactions. So you get incomplete and biased
+  #' top_ranks. 
+  #' 
+  #' To combat this we cut with ties, but that makes our overlap calculations 
+  #' difficult. Percentage-overlap is an intuitive metric but if the two items
+  #' being compared are not the same size the comparison is directional. A x B 
+  #' is not equal to B x A. 
+  #' 
+  #' In order to preserve the intuitive metric but create a bidirectional 
+  #' comparison, we estimate the overlap one would get if one randomly 
+  #' subsampled B to be the same size as A, and then calculated the overlap.
+  #' 
+  #' If A is entirely in B, the average overlap between A and b is P(overlap) *
+  #' length(A) / length(A). P(overlap) is (A in B) / length(B), since we sample
+  #' values from B uniformly. Thus, the average overlap of A and b is 
+  #' (A in B) / length(B). 
+  #'
+  #' @param main_ranks A tibble of of top ranked interactions
+  #' @param comparison_ranks A tibble of top ranked interactions
+  #' @param verbose Should the function describe the overlap to you or not?
+  #'
+  #' @return The overlap (0-1) between the two input frames in contents of the
+  #' LR_ID column, as well as an optional print statement that gives more 
+  #' detail.
+  
+  cellchat_rank_overlap <-function(main_ranks, 
+                                   comparison_ranks, 
+                                   verbose = TRUE) {
+      
+    # calculate overlap between LR_IDs
+    overlap <- sum(comparison_ranks$LR_ID %in% main_ranks$LR_ID)
+    
+    # If the size of both rankings is the same, calculating overlap is simple
+    if (nrow(main_ranks) == nrow(comparison_ranks)) {
+      
+      overlap <- sum(comparison_ranks$LR_ID %in% main_ranks$LR_ID)
+      percentage_overlap <- overlap / nrow(main_ranks)
+      
+    }
+
+    
+    # Otherwise, we determine the average overlap one would achieve when 
+    # downsampling B to the size of A, given that A in entirely in B.
+    if(overlap == nrow(main_ranks)) {
+      
+      percentage_overlap <- overlap / nrow(comparison_ranks)
+      print("cellchat overlap worked")
+      
+    } else {
+      
+      # This case scenario shouldn't come up if top_ranks were preserved.
+      stop("The original cellchat top_ranks could not be found in the diluted
+           cellchat top_ranks. This means that cellchat_rank_overlap() can't 
+           calculate what you want it to.")
+      
+    }
+    
+    
+    # describe the output to the user
+    if (verbose == TRUE) {
+      print(str_glue(""))
+      print(
+        str_glue(
+          "The main ranking and the comparison ranking have ",
+          as.character(overlap),
+          " LR_IDs in common. Which corrsponds to a ",
+          as.character(round(percentage_overlap * 100, 2)),
+          "% overlap."
+        )
+      )
+    }
+    
+    return(percentage_overlap)
+      
+  } #end of function
   
   
 }
