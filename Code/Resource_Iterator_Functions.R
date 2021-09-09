f216008ea428c10cdfd9da300c5087635541d428
LeoBurmedi <49957645+LeoBurmedi@users.noreply.github.com>
2021-09-09 19:52:36 +0200

    Created reformat_Results and two helper functions
    Uses purrr tools instead of for loops now.

--- Code/Resource_Iterator_Functions.R
+++ Code/Resource_Iterator_Functions.R
@@ -12,8 +12,8 @@ wrap_resource_Robustness <-
              "resources_OP"      ,
              "top_ranks_OP"      ,
              "top_ranks_analysis",
-             "metadata"          #,
-           #  "testdata"
+             "metadata"          ,
+             "testdata"
            ),
            
            methods_vector = c(
@@ -39,8 +39,6 @@ wrap_resource_Robustness <-
            liana_warnings  = "divert" # TRUE, FALSE, or "divert"
            
            ) {
-    
-  print(master_seed)
   
   ## Process Dilution Proportions List
   {
@@ -190,3 +188,92 @@ wrap_resource_Robustness <-
 # Should the entire output be redirected to a log file?
 
 # Should liana warnings be visible in output? 
+
+
+
+
+
+
+
+
+flatten_names <- function(three_tier_list, depth) {
+  
+  new_three_tier <-
+    map_depth(three_tier_list, depth, function(two_tier_list) {
+      two_tier_list %>%
+        map2(names(.), function(one_tier_list, one_tier_list_name)
+          rename_list(one_tier_list, one_tier_list_name)) %>%
+        flatten()
+    })
+  
+  return(new_three_tier)
+  
+}
+
+rename_list <- function(list_element, str){
+  
+  new_list <- setNames(list_element,
+                       str_glue("{str}_{names(list_element)}"))
+  
+  return(new_list)
+  
+}
+
+
+
+
+
+reformat_Results <- function(results) {
+  
+  # At most, there are six outputs. They fall into three pairs of two that are
+  # each formatted the same way. Liana results and top ranks are formatted
+  # the same way, resources and top_ranks analysis are formatted the same way,
+  # and metadata and testdata is formatted the same way.
+  
+  
+  # We start by transposing results
+  results <- transpose(results)
+  
+  
+  # This is the segment of the results containing metadata and testdata
+  segment_meta_test <-
+    results[names(results) %in% intersect(names(results),
+                                          c("metadata",
+                                            "testdata"))]
+  
+  # This is the segment of the results containing resources and analysis
+  segment_resources_analysis <-
+    results[names(results) %in% intersect(names(results),
+                                          c("resources_OP",
+                                            "top_ranks_analysis"))]
+  
+  # This is the segment of the results containing ranks and results
+  segment_results_ranks <-
+    results[names(results) %in% intersect(names(results),
+                                          c("liana_results_OP",
+                                            "top_ranks_OP"))]
+  
+  # Format  meta_test
+  segment_meta_test[["metadata"]]  <- segment_meta_test[["metadata"]] %>%
+    map_depth(.depth = 0, transpose) %>%
+    flatten_names(depth = 0)
+  
+  # Format resources_analysis, we transpose at a deeper level
+  segment_resources_analysis <- segment_resources_analysis %>%
+    map_depth(.depth = 1, transpose) %>%
+    flatten_names(depth = 1)
+  
+  # Format results_ranks
+  segment_results_ranks <- segment_results_ranks %>%
+    map_depth(.depth = 1, transpose) %>%
+    map_depth(.depth = 2, transpose) %>%
+    flatten_names(depth = 2)
+    
+  restructured_results <- list(segment_results_ranks, 
+                               segment_resources_analysis,
+                               segment_meta_test) %>%
+    flatten()
+
+   return(restructured_results) 
+  
+}
